ad43fb4c3cff25cab709af99365193b95e92fbc3
qjx <804884073@qq.com>
2018-04-12 19:37:55 +0800

    个人中心

--- src/components/MySpace/MySpace.vue
+++ src/components/MySpace/MySpace.vue
@@ -1,19 +1,22 @@
 <template>
-  <div class="society">
-    我是个人中心
+  <div class="mySpace">
+    <div class="conten">
+      <div class="left_box">
+        <img src="../../assets/img/home1.jpg" alt="">
+      </div>
+      <div class="right_box">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: '',
-    components: {
-
-    },
+    components: {},
     data () {
-      return {
-
-      }
+      return {}
     },
     mounted(){
 
@@ -23,8 +26,38 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  .society{
+  .mySpace {
+    padding-top: 30px;
     height: 100%
-    overflow hidden
+    max-width: 90%;
+    min-width: 90%;
+    overflow hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .conten {
+      background-color: #fff
+      width: 912px;
+      height: 574px;
+      img {
+        width: 150px;
+        height: 140px;
+      }
+    }
+    .left_box {
+      width: 212px;
+      height: 100%;
+      floa: left;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .right_box {
+      width: 700px
+      floa: left;
+      background-color: skyblue
+      height: 100%;
+    }
   }
 </style>
